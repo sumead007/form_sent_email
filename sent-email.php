3c542d7192ed5c755c27ffeb6a1e34f7fceaf1d3
sumead007 <54968854+sumead007@users.noreply.github.com>
2021-12-21 20:01:45 +0700

    Update sent-email.php

--- sent-email.php
+++ sent-email.php
@@ -41,7 +41,7 @@ if (!empty($_POST['token'])) {
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'serviceforbes22@gmail.com';                     //SMTP username
-            $mail->Password   = 'forbes22334499';                               //SMTP password
+            $mail->Password   = '';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
             $mail->CharSet = "utf-8";
